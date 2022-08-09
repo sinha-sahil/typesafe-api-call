48d467ac14938a4f43e4c2dd060a8d7c89ea070b
Sahil Sinha <sahilsinha.dar@gmail.com>
2022-08-09 11:55:17 +0530

    build: added rollup for building & build script

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from "@rollup/plugin-typescript";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      file: "package/index.js",
+      format: "esm",
+      sourcemap: false
+    },
+    plugins: [typescript({
+      tsconfig: "./tsconfig.json"
+    })]
+  }
+]
