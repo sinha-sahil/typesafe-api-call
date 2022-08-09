56d3869daa8219821ab30bb6c0a098ac2f30a706
Sahil Sinha <sahilsinha.dar@gmail.com>
2022-08-09 11:34:43 +0530

    feat: Added callAPI function and types

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { callAPI } from './callAPI';
+export { APISuccess, APIFailure } from './types';
+export type { APIRequest, APIResponse, JSONObject, JSONValue, HttpMethod } from './types';
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export { APIFailure } from './APIFailure';
+export { APISuccess } from './APISuccess';
+export type { APIRequest, HttpMethod, JSONObject, JSONValue } from './APIRequest';
+export type { APIResponse } from './APIResponse';
