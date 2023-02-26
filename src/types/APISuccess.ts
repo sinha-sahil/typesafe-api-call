3985855fbf17505b9751d2b2d87b7f284ae92707
Sahil Sinha <sahilsinha.dar@gmail.com>
2023-02-26 17:29:39 +0530

    feat: Added support for error decoder & agent param
    - Added support for passing errorDecoder & overriding fetch
    - Added support for registering hooks which will be triggered when call starts & ends
    - Refactored types for APIResponse & APIRequest
    - Add support for all params from RequestInit in APIRequest and agent param as well
    - Add time param to API Responses
    - Removed usage of any, used unknown instead
    - Added prettier & eslint for formatting & linting

--- src/types/APISuccess.ts
+++ src/types/APISuccess.ts
@@ -2,14 +2,16 @@
  * @name APISuccess
  * @description Construct an API Success Instance with response
  */
- export class APISuccess<APIResponse> {
+export class APISuccess<APIResponse> {
   readonly statusCode: number;
   readonly status: string;
   readonly response: APIResponse;
+  readonly time: number;
 
-  constructor(statusCode: number, status: string, response: APIResponse) {
+  constructor(statusCode: number, status: string, response: APIResponse, time: number) {
     this.statusCode = statusCode;
     this.status = status;
     this.response = response;
+    this.time = time;
   }
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,46 @@
-export { APIFailure } from './APIFailure';
-export { APISuccess } from './APISuccess';
-export type { APIRequest, HttpMethod, JSONObject, JSONValue } from './APIRequest';
-export type { APIResponse } from './APIResponse';
+import type { APIFailure } from './APIFailure';
+import type { APISuccess } from './APISuccess';
+
+export * from './APISuccess';
+export * from './APIFailure';
+
+export type HttpMethod = 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+
+/**
+ * @name APIRequest
+ * @description Construct API Request which is consumable by the callAPI fn
+ */
+
+export interface APIRequest extends RequestInit {
+  url: URL;
+  method: HttpMethod;
+  agent?: unknown /** To be typed as node-fetch/Agent when available in native fetch */;
+}
+
+/**
+ * @name APIResponse
+ * @description Denotes all possible responses of an API call
+ */
+
+export type APIResponse<ExpectedResponseType> = APISuccess<ExpectedResponseType> | APIFailure;
+
+export type ResponseDecoder<ExpectedResponse> = (rawResponse: unknown) => ExpectedResponse | null;
+
+/**
+ * @description Type of native fetch function
+ */
+export type FetchType = (
+  input: RequestInfo | URL,
+  init?: RequestInit | undefined
+) => Promise<Response>;
+
+/**
+ * @description types for hooks function which will be triggered on start & end of api calls
+ */
+
+export type APICallStartHook = (apiRequest: APIRequest) => void;
+
+export type APICallEndHook<ResponseType> = (
+  apiRequest: APIRequest,
+  apiResponse: APIResponse<ResponseType>
+) => void;
