508fb24bea78157c081d89f12097ffe7e183f7c1
Dhananjay Shettigar <dhananjay.s@juspay.in>
2023-10-23 16:39:53 +0530

    feat: Improved error handling
    - Added error handling based on error instances based on MDN Specifications (Refer: https://developer.mozilla.org/en-US/docs/Web/API/fetch)
    - Bumped build target to ES2022 to allow type-checking key `cause` on Error instance
    - Added errorDetails key to APIFailure object
    - Updated ErrorResponseDecoder param to be null by default

--- src/types/APIFailure.ts
+++ src/types/APIFailure.ts
@@ -1,3 +1,5 @@
+import { ErrorDetails } from ".";
+
 /**
  * @name APIFailure
  * @description Construct an API Error Instance.
@@ -6,13 +8,17 @@
 export class APIFailure<FailureResponseType> {
   readonly errorMessage: string;
   readonly errorCode: number;
-  readonly errorResponse: FailureResponseType | unknown;
+  readonly response: FailureResponseType | null;
+  readonly errorResponse: unknown;
+  readonly errorDetails: ErrorDetails | null;
   readonly time: number;
 
-  constructor(errorMessage: string, errorCode: number, errorResponse: unknown, time: number) {
+  constructor(errorMessage: string, errorCode: number, response: FailureResponseType | null, errorResponse: unknown, time: number, errorDetails: ErrorDetails | null) {
     this.errorMessage = errorMessage;
     this.errorCode = errorCode;
+    this.response = response;
     this.errorResponse = errorResponse;
+    this.errorDetails = errorDetails;
     this.time = time;
   }
 }
